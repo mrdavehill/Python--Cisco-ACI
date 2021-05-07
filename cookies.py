7f2c12080ef354fec7f74960176788eb82c9d804
Dave Hill <dave@davehill.org>
2021-05-07 14:00:23 +0100

    Initial code

--- cookies.py
+++ cookies.py
@@ -1,2 +1,17 @@
+from json import json.dumps
+from requests import requests.post
+from var import apic
+from credentials import *
+
 class Cookies:
-  
+
+  def __init__(self, apic, username, password):
+      self.apic = apic
+      self.username = username
+      self.password = password
+
+  def get_cookies(self):
+      url = apic + '/api/aaaLogin.json'
+      auth = {'aaaUser': {'attributes': {'name': credentials.username, 'pwd': credentials.password}}}
+      authenticate = requests.post(url, data=json.dumps(auth), verify=False)
+      return authenticate.cookies    

7785afb419a573b5863d1f508f4f2a78d990e713
Dave Hill <dave@davehill.org>
2021-05-09 18:49:41 +0100

    More Sunday updates

--- unittest.py
+++ unittest.py
@@ -1,13 +1,18 @@
-#python3.8.4
-#created 9-may-2021
+# python3.8.4
+# created 9-may-2021
 
 import unittest
 
 class apicClass(unittest.TestCase):
 
-    def test_apicClass(self):
-        # create new object on apic
+    def test_apicClassJSON(self):
+        # valid dictionary ready for JSON serialisation
         pass
+
+    def test_apicClassURL(self):
+        # valid url
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()

13bc7f165609504792d4c4864159c600b7cef689
Dave Hill <dave@davehill.org>
2021-05-09 18:45:08 +0100

    Update unittest

--- unittest.py
+++ unittest.py
@@ -1,37 +1,13 @@
-#
-#
-#
+#python3.8.4
+#created 9-may-2021
 
 import unittest
-import requests
-from credentials import *
-from var import apic
-import Cookies
 
-class CRUDTest(unittest.TestCase):
-    def setUp(self):
-        # instantiate class and create cookie
-        self.apic = apic
-        self.username = username 
-        self.password = password
-        self.body = body
-        self.cookies = Cookies(self.apic, self.username, self.password)
-        #return cookies
+class apicClass(unittest.TestCase):
 
-    def create(self):
+    def test_apicClass(self):
         # create new object on apic
-        result = requests.post(url, data=json.dumps(self.body, verify=False, cookies=self.cookies)
-        self.assertEqual(self.result.status_code, 200)
-
-    def test_default_widget_size(self):
-        # read new object on apic
         pass
 
-    def test_default_widget_size(self):
-        # modify the name
-        pass
-
-    def test_default_widget_size(self):
-        # delete object
-       pass
-    
+if __name__ == '__main__':
+    unittest.main()
